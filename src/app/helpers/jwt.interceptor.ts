2e71b9f990a2e384338714a8b1667e0f6b4667f9
Silvio Pablo Cifone <silvio-pablo.cifone-ext@alstomgroup.com>
2021-03-22 16:04:30 +0100

    Rimozione Console Log

--- src/app/helpers/jwt.interceptor.ts
+++ src/app/helpers/jwt.interceptor.ts
@@ -12,14 +12,12 @@ import { environment } from "src/environments/environment";
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
   constructor(private authenticationService: AuthenticationService) {
-    console.log("Inizializzo l'interceptor");
   }
 
   intercept(
     request: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
-    console.log("Ho intercettato la chiamata");
     // add auth header with jwt if user is logged in and request is to api url
     const user = this.authenticationService.userValue;
     const isLoggedIn = user && user.token;
